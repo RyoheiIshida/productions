d7275bfa64177840c5e982e3411b7898b656ce2d
ryohei <mw.iicd@gmail.com>
2022-04-23 19:18:38 +0900

    lesson02
    1st commit

---
+++ lesson02/index.php
@@ -0,0 +1,15 @@
+<?php
+$sample = "Hello World!";
+echo $sample . "\n";
+
+$result = 1 + 8;
+echo $result . "\n";
+
+$result = 6 - 5;
+echo $result . "\n";
+
+$result = 2 * 3;
+echo $result . "\n";
+
+$result = 9 / 3;
+echo $result . "\n";
