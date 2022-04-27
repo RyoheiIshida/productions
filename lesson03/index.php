b31920d1e518cc9714a9b193cb9cefb5e2167539
ryohei <mw.iicd@gmail.com>
2022-04-27 09:22:14 +0900

    lesson03
    1st commit

---
+++ lesson03/index.php
@@ -0,0 +1,19 @@
+<?php
+$num = 29;
+echo $num . "\n";
+$num = 44;
+echo $num . "\n";
+
+$num2 = 100;
+$num2 = $num2 + 10;
+echo $num2 . "\n";
+
+$num3 = 0;
+echo $num3 . "\n";
+echo $num3++ . "\n";
+echo $num3 . "\n";
+
+$num3 = 0;
+echo $num3 . "\n";
+echo ++$num3 . "\n";
+echo $num3 . "\n";
