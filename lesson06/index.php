97b214772f68d3403a0c30740429e90dfccecd7e
ryohei <mw.iicd@gmail.com>
2022-05-19 21:29:47 +0900

    lesson06
    1st commit

---
+++ lesson06/index.php
@@ -0,0 +1,14 @@
+<?php
+$arr[0]["info"] = ["name" => "田中", "division" => "1", "age" => "25"];
+$arr[0]["skill"] = ["lang" => ["PHP", "Ruby"], "fw" => ["Laravel", "CakePHP", "Rails"]];
+$arr[0]["description"] = "バックエンドエンジニア";
+
+$arr[1]["info"] = ["name" => "山田", "division" => "2", "age" => "23"];
+$arr[1]["slill"] = ["lang" => ["HTML", "CSS", "JS"], "fw" => ["Vue.js"]];
+$arr[1]["description"] = "フロントエンドエンジニア";
+
+$arr[2]["info"] = ["name" => "佐藤", "division" => "2", "age" => "20"];
+$arr[2]["slill"] = ["lang" => ["PHP"], "fw" => []];
+$arr[2]["description"] = "PHP初学者";
+
+var_dump($arr);
