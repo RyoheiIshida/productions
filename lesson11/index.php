8791be03b873a92a3e340e621ec534f4aae55777
ryoh <mw.iicd@gmail.com>
2022-07-08 17:41:22 +0900

    lesson11 3td commit
    added echo array too

--- lesson11/index.php
+++ lesson11/index.php
@@ -5,8 +5,15 @@
 * @param string $str string from $_GET
 * @return string $str string throw htmlspecialchars
 */
-function h($str){
-    return htmlspecialchars($str,ENT_QUOTES,"UTF-8");
+function h($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
 }
 
-echo h($_GET['message']);
+echo h($_GET['message']).'<br>';
+
+if ($_GET['hoby'] !== NULL) {
+    foreach ($_GET['hoby'] as $hoby_name) {
+        echo h($hoby_name).'<br>';
+    }
+}
