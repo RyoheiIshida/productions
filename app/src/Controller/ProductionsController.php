3dfb91f5501235cf70f7fa03cc13ecd0626dcf04
ryoh <mw.iicd@gmail.com>
2022-07-15 20:22:13 +0900

    lesson14 25th commit
    changed ProductionsController

--- app/src/Controller/ProductionsController.php
+++ app/src/Controller/ProductionsController.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
+
 
 /**
  * Productions Controller
@@ -34,7 +37,7 @@ class ProductionsController extends AppController
     public function view($id = null)
     {
         $production = $this->Productions->get($id, [
-            'contain' => [],
+            'contain' => ['Stocks', 'Orders'],
         ]);
 
         $this->set('production', $production);
@@ -47,15 +50,29 @@ class ProductionsController extends AppController
      */
     public function add()
     {
+        date_default_timezone_set('Asia/Tokyo');
         $production = $this->Productions->newEntity();
+        $stock = $this->Productions->Stocks->newEntity();
+        $connection = ConnectionManager::get('default');
         if ($this->request->is('post')) {
-            $production = $this->Productions->patchEntity($production, $this->request->getData());
-            if ($this->Productions->save($production)) {
-                $this->Flash->success(__('商品情報が保存されました。'));
+            $connection->begin(); // トランザクション開始
+            try {
+                $production = $this->Productions->patchEntity($production, $this->request->getData());
+                if ($this->Productions->save($production)) {
+                    $stock->productions_id = $production->id;
+                    if ($this->Productions->Stocks->save($stock)) {
+                        $connection->commit(); // 保存に成功したためコミット
+                        $this->Flash->success(__('商品情報を保存しました。'));
 
-                return $this->redirect(['action' => 'index']);
+                        return $this->redirect(['action' => 'index']);
+                    }
+                }
+                $connection->rollback(); // 保存に失敗したためロールバック
+                $this->Flash->error(__('商品情報を保存できませんでした。もう一度試してください。'));
+            } catch (\Exception $e) { // 例外処理
+                $connection->rollback();
+                $this->Flash->error(__('商品情報を保存できませんでした。もう一度試してください。'));
             }
-            $this->Flash->error(__('商品情報が保存されませんでした。もう一度試してください。'));
         }
         $this->set(compact('production'));
     }
@@ -69,17 +86,27 @@ class ProductionsController extends AppController
      */
     public function edit($id = null)
     {
+        date_default_timezone_set('Asia/Tokyo');
         $production = $this->Productions->get($id, [
             'contain' => [],
         ]);
+        $connection = ConnectionManager::get('default');
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $production = $this->Productions->patchEntity($production, $this->request->getData());
-            if ($this->Productions->save($production)) {
-                $this->Flash->success(__('商品情報が保存されました。'));
+            $connection->begin(); // トランザクション開始
+            try {
+                $production = $this->Productions->patchEntity($production, $this->request->getData());
+                if ($this->Productions->save($production)) {
+                    $connection->commit(); // 保存に成功したためコミット
+                    $this->Flash->success(__('商品情報が保存されました。'));
 
-                return $this->redirect(['action' => 'index']);
+                    return $this->redirect(['action' => 'index']);
+                }
+                $connection->rollback(); // 保存に失敗したためロールバック
+                $this->Flash->error(__('商品情報が保存されませんでした。もう一度試してください。'));
+            } catch (\Throwable $e) { // 例外処理
+                $connection->rollback();
+                $this->Flash->error(__('商品情報が保存されませんでした。もう一度試してください。'));
             }
-            $this->Flash->error(__('商品情報が保存されませんでした。もう一度試してください。'));
         }
         $this->set(compact('production'));
     }
@@ -102,5 +129,32 @@ class ProductionsController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+    public function isAuthorized($user)
+    {
+        $action = $this->request->getParam('action');
+
+        if (in_array($action, ['add'])) {
+
+            if ($user['authority'] === '在庫受注社員') {
+                return false;
+            } elseif ($user['authority'] === '在庫発注社員' or $user['authority'] === '在庫発注管理者') {
+                return true;
+            }
+        }
+        if (in_array($action, ['edit'])) {
+            if ($user['authority'] === '在庫発注管理者')
+                return true;
+        }
+
+        if (in_array($action, ['delete'])) {
+            if ($user['authority'] === '在庫発注管理者') {
+                return true;
+            }
+        }
+        $slug = $this->request->getParam('pass.0');
+        if (!$slug) {
+            return false;
+        }
     }
 }
