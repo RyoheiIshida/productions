765b6166cec7dcade07dbe2f81043b5b127bc605
ryoh <mw.iicd@gmail.com>
2022-07-15 20:27:28 +0900

    lesson14 27th commit
    changed OrdersController

--- app/src/Controller/OrdersController.php
+++ app/src/Controller/OrdersController.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Datasource\ConnectionManager;
 
 /**
  * Orders Controller
@@ -50,15 +52,36 @@ class OrdersController extends AppController
      */
     public function add()
     {
-        $order = $this->Orders->newEntity();
-        if ($this->request->is('post')) {
-            $order = $this->Orders->patchEntity($order, $this->request->getData());
-            if ($this->Orders->save($order)) {
-                $this->Flash->success(__('発注情報が保存されました。'));
+        date_default_timezone_set('Asia/Tokyo');
+        if ($this->Auth->user('authority') !== '在庫発注社員' and $this->Auth->user('authority') !== '在庫発注管理者') {
+            $this->Flash->error(__('権限がありません。管理者へお問い合わせください。'));
+            return $this->redirect(['action' => 'index']);
+        }
 
-                return $this->redirect(['action' => 'index']);
+        $order = $this->Orders->newEntity();
+        $connection = ConnectionManager::get('default');
+        if ($this->request->is('post')) {
+            $connection->begin(); // トランザクション開始
+            try {
+                if ($this->request->getData('status') !== '発注確認') {
+                    $this->Flash->error(__('初期ステータスは発注確認を選択してください。'));
+                    return $this->redirect(['action' => 'add']);
+                }
+                $order = $this->Orders->patchEntity($order, $this->request->getData());
+                $stock = $this->Orders->Stocks->findByProductionsId($this->request->getData()['productions_id'])->first();
+                $order['stocks_id'] = $stock['id'];
+                if ($this->Orders->save($order)) {
+                    $connection->commit(); // 保存に成功したためコミット
+                    $this->Flash->success(__('発注情報が保存されました。'));
+
+                    return $this->redirect(['action' => 'index']);
+                }
+                $connection->rollback(); // 保存に失敗したためロールバック
+                $this->Flash->error(__('発注情報が保存されました。'));
+            } catch (\Exception $e) {
+                $connection->rollback(); // 保存に失敗したためロールバック
+                $this->Flash->error(__('発注情報が保存されませんでした。もう一度試してください。'));
             }
-            $this->Flash->error(__('発注情報が保存されませんでした。もう一度試してください。'));
         }
         $stocks = $this->Orders->Stocks->find('list', ['limit' => 200]);
         $productions = $this->Orders->Productions->find('list', ['limit' => 200]);
@@ -74,17 +97,46 @@ class OrdersController extends AppController
      */
     public function edit($id = null)
     {
+        date_default_timezone_set('Asia/Tokyo');
         $order = $this->Orders->get($id, [
-            'contain' => [],
+            'contain' => ['stocks'],
         ]);
+        $connection = ConnectionManager::get('default');
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $order = $this->Orders->patchEntity($order, $this->request->getData());
-            if ($this->Orders->save($order)) {
-                $this->Flash->success(__('発注情報が保存されました。'));
+            $connection->begin(); // トランザクション開始
+            try {
+                if (
+                    !$this->isEditableStatus(
+                        $this->Auth->user('authority'),
+                        $this->Orders->get($id)['status'],
+                        $this->request->getData('status')
+                    )
+                ) {
+                    $this->Flash->error(__('権限がありません。管理者へお問い合わせください。'));
+                    return $this->redirect(['action' => 'index']);
+                }
 
-                return $this->redirect(['action' => 'index']);
+                $order = $this->Orders->patchEntity($order, $this->request->getData());
+                if ($this->request->getData('status') === '発注受け取り済み') {
+                    $stock = $this->Orders->Stocks->get($order['stocks_id']);
+                    $stock['stock_quantity'] = $stock['stock_quantity'] + $order['order_quantity'];
+                    if ($this->Orders->save($order, ['associated' => ['Stocks']]) and $this->Orders->Stocks->save($stock)) {
+                        $connection->commit();
+                        $this->Flash->success(__('発注情報が保存されました。'));
+                        return $this->redirect(['action' => 'index']);
+                    }
+                }
+                if ($this->Orders->save($order)) {
+                    $connection->commit(); // 保存に成功したためコミット
+                    $this->Flash->success(__('発注情報が保存されました。'));
+                    return $this->redirect(['action' => 'index']);
+                }
+                $connection->rollback(); // 保存に失敗したためロールバック
+                $this->Flash->error(__('発注情報が保存されませんでした。もう一度試してください。'));
+            } catch (\Exception $e) {
+                $connection->rollback(); // 保存に失敗したためロールバック
+                $this->Flash->error(__('発注情報が保存されませんでした。もう一度試してください。'));
             }
-            $this->Flash->error(__('発注情報が保存されませんでした。もう一度試してください。'));
         }
         $stocks = $this->Orders->Stocks->find('list', ['limit' => 200]);
         $productions = $this->Orders->Productions->find('list', ['limit' => 200]);
@@ -109,5 +161,48 @@ class OrdersController extends AppController
         }
 
         return $this->redirect(['action' => 'index']);
+    }
+
+
+    /**
+     * @info whether user can or can't edit status of orders table 
+     * @param string $authority users->authority,
+     * @param string $bs before status.
+     * @param string $as after status.
+     * @return bool true means status is editable.
+     */
+    public function isEditableStatus($authority, $bs, $as)
+    {
+        if ($authority === '在庫発注管理者') {
+            return true;
+        } elseif ($authority === '在庫発注社員' and $bs === '発注状態' and $as === '発注済み') {
+            return true;
+        } elseif ($authority === '在庫受注社員' and $bs === '発注状態' and $as === '発注済み') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function isAuthorized($user)
+    {
+        $action = $this->request->getParam('action');
+        if (in_array($action, ['add'])) {
+            if ($user['authority'] === '在庫発注管理者' or $user['authority'] === '在庫発注社員') {
+                return true;
+            } 
+        } 
+
+        if (in_array($action, ['edit'])) {
+                return true;
+        }
+        if (in_array($action, ['delete'])) {
+            if ($user['authority'] === '在庫発注管理者') {
+                return true;
+            }
+        }
+        $slug = $this->request->getParam('pass.0');
+        if (!$slug) {
+            return false;
+        }
     }
 }
